f595afd28106c74ffb5b9fd2a781cfe94239d2e9
AswathyRenju <33021174+AswathyRenju@users.noreply.github.com>
2018-06-09 15:08:32 +0530

    Update dataService.js

--- js/dataService.js
+++ js/dataService.js
@@ -1,5 +1,6 @@
 imageListApp.service('dataService', function($http){
     this.pageNumRequested=1;
+    this.prevPage=0;
 
     this.getData = function () {
     return $http.get("./api/CONTENTLISTINGPAGE-PAGE" + this.pageNumRequested + ".json");
