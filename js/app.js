f0f903722552127dd53ea0c1161d662e0d6d2f04
AswathyRenju <33021174+AswathyRenju@users.noreply.github.com>
2018-06-10 20:11:41 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ imageListApp.controller('ImageListController', function ($scope, dataService) {
         .success(function (response) {
           $scope.pageTitle = response.page.title;
           $scope.totalRecordCount = parseInt(response.page['total-content-items'], 10);
-          dataService.prevPage=dataService.pageNumRequested;
+          dataService.prevPageNumRequested=dataService.pageNumRequested;
           dataService.pageNumRequested++;
           var data = response.page['content-items'].content;
           if ($scope.dataList.length) {
